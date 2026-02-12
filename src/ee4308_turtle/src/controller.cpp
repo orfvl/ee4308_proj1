0a1cfd3735f874b96a754b86f94ed69a04634e84
orfvl <oscarvonloehneysen@gmail.com>
2026-02-12 21:36:34 +0800

    feat(controller): added min lookahead_dist

--- src/ee4308_turtle/src/controller.cpp
+++ src/ee4308_turtle/src/controller.cpp
@@ -130,7 +130,7 @@ namespace ee4308::turtle
         // RCLCPP_INFO_STREAM(node_->get_logger(), "Curvature: " << curvature << ", Dist: " << dist << "lookahead_dist_: " << desired_lookahead_dist_  );
         // Calculate the curvature heuristic. 
         // RCLCPP_INFO_STREAM(node_->get_logger(), "Before curvature adjustment, desired_linear_vel: " << desired_linear_vel);
-        desired_linear_vel = ( std::abs(curvature) > this->curvature_threshold_) ? desired_linear_vel * this->curvature_threshold_/curvature : desired_linear_vel; 
+        desired_linear_vel = ( std::abs(curvature) > this->curvature_threshold_) ? desired_linear_vel * this->curvature_threshold_/std::abs(curvature) : desired_linear_vel; 
 
         // RCLCPP_INFO_STREAM(node_->get_logger(), "After curvature adjustment, desired_linear_vel: " << desired_linear_vel);    
         // Calculate the obstacle heuristic.
@@ -139,8 +139,8 @@ namespace ee4308::turtle
 
         // RCLCPP_INFO_STREAM(node_->get_logger(), "After proximity adjustment, desired_linear_vel: " << desired_linear_vel);
         
-        // // Vary the lookahead.
-        desired_lookahead_dist_ = this->lookahead_gain_ * std::abs(desired_linear_vel);
+        // Vary the lookahead.
+        desired_lookahead_dist_ = std::max(0.1, this->lookahead_gain_ * std::abs(desired_linear_vel));
 
         
         // Constrain ω to within the largest allowable angular speed.
